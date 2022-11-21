f3dff48354cff54f3a7c21dc4efcc8b01a9f56b4
Karol Moroz <k.j.moroz@gmail.com>
2022-11-22 00:50:36 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EmailTldValidator.MixProject do
   def project do
     [
       app: :email_tld_validator,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
